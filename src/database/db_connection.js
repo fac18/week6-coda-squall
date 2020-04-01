b9649b3024ed83ca23fbd543e2f054d3c8d5a7f4
edificex <edificex@posteo.net>
2020-04-01 10:29:31 +0100

    rework db_connection file to work in test environment without production DB_URL specified in .env file

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -1,28 +1,25 @@
-const { Pool } = require("pg");
-const url = require("url");
-require("env2")("./.env");
+const { Pool } = require('pg')
+const url = require('url')
+require('env2')('./.env')
 
-//Etablishing test for database
-let DB_URL = process.env.DB_URL;
+// establish environment and assign database URL accordingly
+DB_URL =
+  process.env.NODE_ENV === 'test' ? process.env.TEST_DB_URL : process.env.DB_URL
 
-if (process.env.NODE_ENV === "test") {
-  DB_URL = process.env.TEST_DB_URL;
-}
+if (!DB_URL) throw new Error('Environment variable DB_URL must be set!')
 
-if (!DB_URL) throw new Error("Environment variable DB_URL must be set!");
-
-const params = url.parse(process.env.DB_URL);
-const [user, password] = params.auth.split(":");
+const params = url.parse(DB_URL)
+const [user, password] = params.auth.split(':')
 
 const options = {
   host: params.hostname,
   port: params.port,
-  database: params.pathname.split("/")[1],
+  database: params.pathname.split('/')[1],
   max: process.env.DB_MAX_CONNECTIONS || 2,
   user,
   password,
-  ssl: params.hostname !== "localhost"
-};
+  ssl: params.hostname !== 'localhost',
+}
 
-const dbConnection = new Pool(options);
-module.exports = dbConnection;
+const dbConnection = new Pool(options)
+module.exports = dbConnection
