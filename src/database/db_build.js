2f38ba8bd4430dd4fadb3c8e2610f29614310960
edificex <edificex@posteo.net>
2020-04-01 10:30:57 +0100

    changes

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -1,10 +1,10 @@
-const fs = require("fs");
-const dbConnection = require("./db_connection");
-const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+const fs = require('fs')
+const dbConnection = require('./db_connection')
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString()
 
 const runDbBuild = cb => {
-  dbConnection.query(sql, cb);
-};
+  dbConnection.query(sql, cb)
+}
 
 // runDbBuild((err, res) => {
 //   if (err) {
@@ -14,4 +14,4 @@ const runDbBuild = cb => {
 //   }
 // });
 
-module.exports = runDbBuild;
+module.exports = runDbBuild
